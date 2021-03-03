9c1b0a71fcfa55d4e50ef31cfa8553d6fec324dd
dotvirus <33938500+dotvirus@users.noreply.github.com>
2021-03-03 23:58:11 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
+#[get("/")]
 async fn hello_world() -> impl Responder {
   HttpResponse::Ok().body("Hello world!")
 }
@@ -7,7 +8,7 @@ async fn hello_world() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
   println!("Running actix web boilerplate, visit http://localhost:8080");
-  HttpServer::new(|| App::new().route("/", web::get().to(hello_world)))
+  HttpServer::new(|| App::new().service(hello_world))
     .bind("127.0.0.1:8080")?
     .run()
     .await
